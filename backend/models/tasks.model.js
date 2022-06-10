450f40c1e3773fb19dde09343e4c79104b2f2993
SafaeQ <safaedani009@gmail.com>
2022-06-10 22:43:37 +0100

    💄 add schema tasks

--- backend/models/tasks.model.js
+++ backend/models/tasks.model.js
@@ -5,5 +5,13 @@ const Tasks = new mongoose.Schema({
         type: String,
         required: true
     },
-    
+    discription: {
+        type: String,
+        required: true
+    },
+    type: {
+
+    }
 })
+
+module.exports = mongoose.model('Task', Tasks)
