fe3e880dda05f8fb579775e2a2bab313a18991a9
SafaeQ <safaedani009@gmail.com>
2022-06-10 20:13:30 +0100

    ✅ strating MVC structure

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,27 @@
-const express = require('express')
-
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const db = require('./connection/db')
+require('dotenv').config();
+
+const PORT = process.env.PORT || 9898
 
 // set up express app
 const app = express()
 
 
-const PORT = process.env.PORT || 9898
-
-
 // middlewares
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(cors({ origin: true }))
+
 app.use('/', (req, res)=>{
     res.send('hello from backend')
 })
 
+// routes
 
 // listen for requests
 db()
