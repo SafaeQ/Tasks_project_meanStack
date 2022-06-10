59de9ba72f3f7b025874f76fb05c24a549bdff8e
SafaeQ <safaedani009@gmail.com>
2022-06-10 22:10:54 +0100

    🎉  add bcrypt to login func

--- backend/controllers/auth/usersAuth.js
+++ backend/controllers/auth/usersAuth.js
@@ -30,4 +30,22 @@ const signup = async (req, res) =>{
     }
 }
 
+const login = async (req, res) => {
+    try {
+        const { email, password} = req.body
+
+        const user = await User.findOne({ email })
+
+        if (!user) return res.status(409).send(`🤷‍♂️Email Incorrect / Not Found! Please Register First.`)
+
+        const validPassword = bcrypt.compare(password, user.password)
+
+        if (!validPassword) return res.status(409).send(' 🤷‍♂️Password not correct')
+
+        
+    } catch (error) {
+        
+    }
+}
+
 module.exports = { signup, }
