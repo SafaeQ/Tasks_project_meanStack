8c6a3906c98f88c0d842b30cded99da85d3d3c9a
SafaeQ <safaedani009@gmail.com>
2022-06-10 19:54:05 +0100

    ✅  add conncetion

--- backend/connection/db.js
+++ backend/connection/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 
 function db(){
-    mongoose.connect(process.env.DATABASE, {useNewUrlParser : true} err => {
+    mongoose.connect(process.env.DATABASE, {useNewUrlParser : true}, err => {
         console.log('👏 Connected with db');
     })
 }
