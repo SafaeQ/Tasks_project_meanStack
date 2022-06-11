9ceb06d6fc0e8d37cfaa373df966a611c801052a
SafaeQ <safaedani009@gmail.com>
2022-06-11 16:34:53 +0100

    📝 add routes login sign up

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { SignupComponent } from './authComponent/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authComponent/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:'login',  pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
