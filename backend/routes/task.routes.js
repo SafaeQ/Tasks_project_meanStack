5d9c5dd715512a24a58e99a6872c3adcf18c0b47
SafaeQ <safaedani009@gmail.com>
2022-06-11 10:32:09 +0100

    🔥  finish task routes

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,1 +1,15 @@
 const taskRouter = require('express').Router()
+
+const { getAllTask, createTask, deleteTask, getTaskById } = require('../controllers/Task.controller')
+
+
+    taskRouter.get('/', getAllTask)
+
+    taskRouter.get('/add', createTask)
+
+    taskRouter.get('/delete', deleteTask)
+
+    taskRouter.get('/:id', getTaskById)
+    
+
+module.exports = taskRouter;
