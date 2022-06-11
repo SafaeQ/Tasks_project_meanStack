1916af28bcfcb8a37a997bac874d9aa8bd0f44cb
SafaeQ <safaedani009@gmail.com>
2022-06-11 12:09:10 +0100

    🎉  set up func is auth

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -2,4 +2,13 @@ const jwt = require('jsonwebtoken')
 
 const checkAuth = async (req, res, next) => {
     
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) return res.sendStatus(401);
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) return res.sendStatus(401)
+
+    
 }
