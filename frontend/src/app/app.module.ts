d26b72ac8565a8176d57465787a36e5643f3740d
SafaeQ <safaedani009@gmail.com>
2022-06-11 14:57:02 +0100

    🎉  generate login components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './authComponent/signup/signup.component';
+import { LoginComponent } from './authComponent/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
