160893c914d128dddb60b9a27ab973f94a8fb7a7
SafaeQ <safaedani009@gmail.com>
2022-06-12 14:44:33 +0100

    😑 create protected router ✔

--- frontend/src/app/guard/auth.guard.ts
+++ frontend/src/app/guard/auth.guard.ts
@@ -1,14 +1,22 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(public auth: AuthService, public router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.auth.isLoggedIn){
+        window.alert('access denied')
+        this.auth.navigateToLogin
+      }
     return true;
   }
 
