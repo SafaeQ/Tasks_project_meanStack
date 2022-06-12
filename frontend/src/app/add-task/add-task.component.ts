b1c9347565d1a0806f1241a28132ff2c5a391b28
SafaeQ <safaedani009@gmail.com>
2022-06-12 21:16:21 +0100

    🐱‍🏍 define my property

--- frontend/src/app/add-task/add-task.component.ts
+++ frontend/src/app/add-task/add-task.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
+  label:string = "";
+  date:Date = new Date();
+  description:string = "";
+  type:string = "";
+  // completed: Boolean = "";
+
   constructor() { }
 
   ngOnInit(): void {
