04281454157f69c0a640b6587a48460f79417653
SafaeQ <safaedani009@gmail.com>
2022-06-12 23:45:09 +0100

    🎉😜add select types

--- frontend/src/app/add-task/add-task.component.ts
+++ frontend/src/app/add-task/add-task.component.ts
@@ -1,11 +1,16 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
+interface TypeTask {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
+
 export class AddTaskComponent implements OnInit {
 
   label:string = "";
@@ -13,6 +18,12 @@ export class AddTaskComponent implements OnInit {
   description:string = "";
   type:string = "";
   completed: Boolean = false;
+
+  types: TypeTask[] = [
+    {value: 'work-0', viewValue: 'Work'},
+    {value: 'personnel-1', viewValue: 'Personnel'},
+    {value: 'growth-2', viewValue: 'Growth'},
+  ];
 
   constructor( @Inject(MAT_DIALOG_DATA) public data:any) {
     this.label = data.name
