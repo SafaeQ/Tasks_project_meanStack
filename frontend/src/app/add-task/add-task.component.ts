cdd25500b0946f94f4b643515ec3b2f38e447357
SafaeQ <safaedani009@gmail.com>
2022-06-12 21:40:32 +0100

    ✨ type on No click

--- frontend/src/app/add-task/add-task.component.ts
+++ frontend/src/app/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-task',
@@ -7,13 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
-  label:string = "";
-  date:Date = new Date();
-  description:string = "";
-  type:string = "";
-  // completed: Boolean = "";
+  constructor( public dialogRef: MatDialogRef<AddTaskComponent>) { }
 
-  constructor() { }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
   ngOnInit(): void {
   }
