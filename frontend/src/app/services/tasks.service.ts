dc19a0b812fb2d5b3e60860a0addefaa3ffcbc9c
SafaeQ <safaedani009@gmail.com>
2022-06-13 11:35:46 +0100

    💄  create all tasks

--- frontend/src/app/services/tasks.service.ts
+++ frontend/src/app/services/tasks.service.ts
@@ -1,6 +1,8 @@
+import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,8 @@ export class TasksService {
 
   constructor(private http:HttpClient, private router: Router,) { }
 
-
+  public getAllTasks(){
+    let url: string = `${environment.api_Url}/`;
+    return this.http.get<any>(url)
+  }
 }
