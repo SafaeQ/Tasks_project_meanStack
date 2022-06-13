252b3ef06a5f66c9dbe5a918934c3349f4085024
SafaeQ <safaedani009@gmail.com>
2022-06-13 21:55:31 +0100

    ✌🐱‍👤 remove line unusable 👀

--- frontend/src/app/services/tasks.service.ts
+++ frontend/src/app/services/tasks.service.ts
@@ -1,16 +1,14 @@
-import { AuthService } from './auth.service';
 import { Observable } from 'rxjs';
 import { environment } from './../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor(private http:HttpClient, private router: Router, private auth: AuthService) { }
+  constructor(private http:HttpClient) { }
 
 
   public getAllTasks(): Observable<any>{
