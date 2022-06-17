538a79eb6d7cbdaca1b9a70903f7ccbbcf6d7f3b
SafaeQ <safaedani009@gmail.com>
2022-06-17 09:39:35 +0100

    ♻️ Cleaning project files ♻️

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { SignupComponent } from './authComponent/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authComponent/login/login.component';
-import { AddTaskComponent } from './add-task/add-task.component';
-import { EditTaskComponent } from './edit-task/edit-task.component';
+
 
 const routes: Routes = [
   {path: '',redirectTo:'login',  pathMatch: 'full'},
